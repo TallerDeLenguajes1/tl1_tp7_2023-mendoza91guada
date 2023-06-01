ffa450ca473748e04a47c087749b74694290ff02
Guadalupe <mendoza91guada@gmail.com>
2023-06-01 19:54:33 -0300

    Commit general

---
+++ Calculadora.cs
@@ -0,0 +1,42 @@
+namespace EspacioCalculadora
+{
+    public class Calculadora
+    {
+        private double dato;
+
+        public void Sumar(double termino)
+        {
+            dato +=termino;
+        }
+
+        public void Restar (double termino)
+        {
+            dato -= termino;
+        }
+
+        public void Multiplicar (double termino)
+        {
+            dato *= termino;
+        }
+
+        public void Dividir (double termino)
+        {
+            dato /= termino;
+        }
+
+        public void Limpiar()
+        {
+            dato = 0;
+        }
+        
+        public double Resultado
+        {
+            get
+            {
+                return dato;
+            }
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using EspacioCalculadora;
+
+Calculadora operaciones = new Calculadora();
+
+/*operaciones.Sumar(20);
+operaciones.Sumar(10);
+Console.WriteLine(operaciones.Resultado);*/
+
+Console.WriteLine("----CALCULADORA----");
+
+Console.WriteLine("Presiona el dígito correspondiente a la operación a realizar:\n");
+Console.WriteLine("1- SUMA \n 2- RESTA \n 3- MULTIPLICAR \n 4- DIVIDIR \n 0- LIMPIAR \n");
